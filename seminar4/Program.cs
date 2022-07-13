7ce5b0f5f5625bb2e53b662264f35eaa84ae522c
Ivan Drobyshev <drobyshev_ivan00@mail.ru>
2022-07-13 21:20:18 +0500

    added seminar5

---
+++ seminar4/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+    namespace Project4
+    {
+        class MainClass
+        {
+            public static void Main (string[] args)
+            {   
+                
+                while (true)
+                {
+                    string prog = "";
+
+                    Console.Clear();
+                    Console.WriteLine("Выберите программу:");
+                    Console.WriteLine("\n1 - Возводит число А в натуральную степень В.");
+                    Console.WriteLine("2 - Считает сумму цифр в чиле.");
+                    Console.WriteLine("3 - Квадрыты случайных чисел.");
+                    Console.WriteLine("\t(Для выхода введите 'q')");
+
+                    prog = Console.ReadLine();
+                    
+                    if (prog == "1") {Program1();}
+                    if (prog == "2") {Program2();}
+                    if (prog == "3") {Program3();}
+                    
+                    
+                    if (prog == "q") 
+                    {   
+                        Console.WriteLine("\nХорошего дня!;)");
+                        break;
+                    }
+
+                    
+                }
+                  
+            }
+            public static void Program1()
+            {  
+                string stop = "";
+                
+                while (stop != "n")
+                {   
+                    Console.Clear();
+                    Console.WriteLine("Возвидение числа А в натуральную степень В.");
+                    Console.WriteLine("\nВведите значение числа А и B");
+                    var number  = Console.ReadLine().Split(',');
+
+
+
+                    double sqert = Math.Pow(Convert.ToDouble(number[0]), Convert.ToDouble(number[1]));
+                    Console.WriteLine($"{number[0]},{number[1]} -> {sqert}");
+
+                    Console.WriteLine("Повторить операцию введите y|n?");
+                    stop = Console.ReadLine();
+
+                }
+            }
+            public static void Program2()
+            {   
+                string stop2 = null;
+                while(stop2 != "n")
+                {
+                    Console.Clear();
+                    
+                    string number = null;
+                    int count = 0;
+
+                    Console.WriteLine("Подсчет суммы цифр в числе");
+                    Console.WriteLine("\nВведите число");
+                    
+                    number = Console.ReadLine();
+                    int len = number.Length;
+
+                    for (int i = 0; i<len; i++)
+                    {   
+                        Console.WriteLine(number[i]);
+                        count = count + int.Parse(number[i].ToString());
+                    }
+
+                    Console.WriteLine($"\n{number} -> {count}");
+                    Console.WriteLine("\nПовторить операцию (y|n)?");
+                    stop2 = Console.ReadLine();
+
+                }
+            }
+            public static void Program3()
+            {   
+                string stop = "";
+                while (stop != "n")
+                {
+                    Console.WriteLine("Program3");
+
+                    Random rand = new Random();
+
+                    double [] numbers = new double [rand.Next(5, 11)];
+
+                    for (int i = 0; i < numbers.Length; i++)
+                    {   
+                        numbers[i] = Math.Pow(rand.Next(1, 41), 2);
+
+                    }
+                    foreach (double d in numbers)
+                    {
+                        Console.WriteLine(d);
+                    }
+                    Console.WriteLine("Повторить операци (y|n)?");
+                    stop = Console.ReadLine();
+            
+                }
+            }
+        }
+    }
---
+++ seminar5/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+    namespace Program5
+    {
+        class MainClass
+        {
+            public static void Main(string [] args)
+            {
+                while (true)
+                {   
+                    string prog = "";
+
+                    Console.Clear();
+                    Console.WriteLine("Выберите программу:");
+                    Console.WriteLine("\n1 - Количеcтво четных чисел в массиве.");
+                    Console.WriteLine("2 - Сумма элементов массива, стоящих на нечетных позициях.");
+                    Console.WriteLine("3 - Разница между максимальным и минимальным элементами массива.");
+                    Console.WriteLine("\t(Для выхода введите 'q')");
+                    prog = Console.ReadLine();
+
+                    if (prog == "1") {Program1();}
+                    if (prog == "2") {Program2();}
+                    if (prog == "3") {Program3();}
+                    if (prog == "q") {break;}
+                }
+            }
+            public static void Program1()
+            {   
+
+                while (true)
+                {
+                    Console.Clear();
+                    Random rand = new Random();
+
+                    int count = 0;
+                    Console.WriteLine("Сколько элементов должно быть в массиве?");
+                    count = Convert.ToInt32(Console.ReadLine());
+
+                    int [] numbers_list = new int[count];
+                    Console.WriteLine($"Массив содержит {count} чисел:");
+
+                    for (int i = 0; i < numbers_list.Length; i++)
+                    {
+                        numbers_list[i] = rand.Next(99, 999);
+                        Console.WriteLine(numbers_list[i]);
+                    }
+
+                    Console.WriteLine();
+                    Console.WriteLine("Четные числа:");
+
+                    for (int j = 0; j < numbers_list.Length; j++)
+                    {
+                        if (numbers_list[j] % 2 == 0)
+                        {
+                            Console.WriteLine($"{numbers_list[j]}");
+                        }
+                       
+                    }
+
+                    string stop = "";
+
+                    Console.WriteLine("\nПовторить операцию введите y|n?");
+                    stop = Console.ReadLine();
+
+                    if (stop == "y") {continue;}
+                    else {break;}
+
+                }
+
+                
+            }
+            public static void Program2()
+            {
+                while (true)
+                {
+                    Console.Clear();
+                    Random rand = new Random();
+
+                    int totoal = 0;
+                    int count = 0;
+                    Console.WriteLine("Сколько элементов должно быть в массиве?");
+                    count = Convert.ToInt32(Console.ReadLine());
+
+                    int [] numbers_list = new int[count];
+                    Console.WriteLine($"Массив содержит {count} чисел:"); 
+
+                    for (int i = 0; i < numbers_list.Length; i++)
+                    {
+                        numbers_list[i] = rand.Next(0, 999);
+                        Console.WriteLine($"\t{numbers_list[i]}");
+                    }
+                    
+                    Console.WriteLine();
+                    Console.WriteLine("Числа с нечетными индексами:");
+
+                    for (int j = 0; j < numbers_list.Length; j ++)
+                    {
+                        if (j % 2 == 0) {Console.WriteLine($"\t{numbers_list[j]}");}
+                        totoal += numbers_list[j];
+                    }
+                    
+                    Console.WriteLine($"\nCумма чисел под нечетными индексами = {totoal}");
+                    string stop = "";
+
+                    Console.WriteLine("\nПовторить операцию введите y|n?");
+                    stop = Console.ReadLine();
+
+                    if (stop == "y") {continue;}
+                    else {break;}                    
+                }
+            }
+            public static void Program3()
+            {
+               while (true)
+                {
+                    Console.Clear();
+                    Random rand = new Random();
+
+                    int count = 0;
+                    Console.WriteLine("Сколько элементов должно быть в массиве?");
+                    count = Convert.ToInt32(Console.ReadLine());
+
+                    int [] numbers_list = new int[count];
+                    Console.WriteLine($"Массив содержит {count} чисел:");
+
+                    for (int i = 0; i < numbers_list.Length; i++)
+                    {
+                        numbers_list[i] = rand.Next(99, 999);
+                        Console.WriteLine($"{numbers_list[i]}");
+                    }
+
+                    Console.WriteLine();
+                    
+                    int max = numbers_list.Max();
+                    int min = numbers_list.Min();
+                    int result = max - min;
+
+                    Console.WriteLine($"Разница между максимальным {max} и минимальным {min} = {result}");
+
+                    string stop = "";
+
+                    Console.WriteLine("\nПовторить операцию введите y|n?");
+                    stop = Console.ReadLine();
+
+                    if (stop == "y") {continue;}
+                    else {break;}
+
+                } 
+            }
+
+        }
+    }
